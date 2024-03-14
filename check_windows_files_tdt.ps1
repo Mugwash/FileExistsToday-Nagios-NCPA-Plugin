7cce3ce7673023a31562ee600261b59846d8fcc0
jack grime <jackgrime@mugstudios.co.uk>
2024-03-14 15:11:12 +0000

    added functionality to specify if it's the day before today for example

--- check_windows_files_tdt.ps1
+++ check_windows_files_tdt.ps1
@@ -22,31 +22,29 @@ PS> .\check_windows_files.ps1 -checkPath C:\\Monitoring\\MyDir\\somelogfile.log 
 #>
 
 param (
-    [Parameter(Mandatory=$true)][string]$checkPath,
-    [Parameter(Mandatory=$false)][string]$dateTimeFormat,
-    [Parameter(Mandatory=$true)][float]$startTime,
-    [Parameter(Mandatory=$true)][float]$endTime,
-    [Parameter(Mandatory=$false)][string]$filename,
+    [Parameter(Mandatory=$true)][string]$checkPath,  # The path to the file or directory you wish to monitor
+    [Parameter(Mandatory=$false)][string]$dateTimeFormat,  # The format of the date and time
+    [Parameter(Mandatory=$true)][float]$startTime,  # The start time in 24-hour format
+    [Parameter(Mandatory=$true)][float]$endTime,  # The end time in 24-hour format
+    [Parameter(Mandatory=$false)][string]$filename,  # The filename to check (optional)
     
-    
-    [Parameter(Mandatory=$false,ParameterSetName='exists')][switch]$exists,
-    [Parameter(Mandatory=$false,ParameterSetName='exists')][switch]$shouldnotexist,
+    [Parameter(Mandatory=$false,ParameterSetName='exists')][switch]$exists,  # The file specified should exist
+    [Parameter(Mandatory=$false,ParameterSetName='exists')][switch]$shouldnotexist,  # The file specified should not exist
 
-
-    [Parameter(Mandatory=$false)][switch]$today,
-    [Parameter(Mandatory=$false)][int]$var_day
+    [Parameter(Mandatory=$false)][switch]$today,  # Check for a file that contains today's date
+    [Parameter(Mandatory=$false)][int]$var_day  # The number of days to add to the current date (optional)
 )
 
-#Setting global error action preference
-#Will probably revisit this when I add the verbose switch
+# Setting global error action preference
+# Will probably revisit this when I add the verbose switch
 $ErrorActionPreference = "SilentlyContinue"
 [int]$exitCode = 2
 [string]$exitMessage = "CRITICAL: something wicked happened"
 [decimal]$version = 1.3
 
 function sanitizePath {
-    #TODO: Need to figure out how to sanitize a path in Powershell.
-    #I want people to be able to do C:\Path\To\File, not C:\\Path\\To\\File
+    # TODO: Need to figure out how to sanitize a path in Powershell.
+    # I want people to be able to do C:\Path\To\File, not C:\\Path\\To\\File
     param (
         [Parameter(Mandatory=$true)][string]$checkPath
     )
@@ -66,22 +64,24 @@ function checkFileExists {
     $returnBool = $false
 
     foreach ($extension in $fileExtensions) {
-    $tempPath = "$path\\$formattedDateTime$remainingFilename$extension"
-    Write-Host "File extension: $tempPath"
-    if (Get-CimInstance -ClassName CIM_LogicalFile `
-        -Filter "Name='$tempPath'" `
-        -KeyOnly `
-        -Namespace root\cimv2) {
+        $tempPath = "$path\\$formattedDateTime$remainingFilename$extension"
+        Write-Host "File extension: $tempPath"
+        if (Get-CimInstance -ClassName CIM_LogicalFile `
+            -Filter "Name='$tempPath'" `
+            -KeyOnly `
+            -Namespace root\cimv2) {
 
-        $returnBool = $true
+            $returnBool = $true
 
-    }}
+        }
+    }
 
     return $returnBool
 }
 
 ## MAIN SCRIPT ##
-##possible file extensions array
+
+## Possible file extensions array
 $fileExtensions = @(
     ".txt",
     ".doc",
@@ -100,49 +100,55 @@ $fileExtensions = @(
     ".rar"
     # Add more extensions as needed
 )
-#Set the date and times  
+
+# Set the date and times  
 $currentDate = Get-Date
 $formattedTime = $currentDate.ToString("HH.mm")/1
 $formattedDate = $currentDate.AddDays($var_day).ToString($dateTimeFormat)
 write-host "Formatted Date: $formattedDate"
-#check if the user has specified the -today switch
+
+# Check if the user has specified the -today switch
 If ($today -eq $true){
     $formattedDate = $currentDate.ToString($dateTimeFormat)
 }
-#check if the current time is between the start and end times
+
+# Check if the current time is between the start and end times
 If ($formattedTime -ge $startTime -and $formattedTime -le $endTime)
 {
-    #confirm that the current time is applicable
+    # Confirm that the current time is applicable
     Write-Output "$formattedTime time is between $startTime and $EndTime"
-    #check if the file exists
+    
+    # Check if the file exists
     if ($exists -eq $true) {
-    if (checkFileExists -path $checkpath -formattedDateTime $formattedDate -remainingFilename $filename) {
-        if ($shouldnotexist -eq $true) {
-            $exitMessage = "CRITICAL: I found the file $checkpath\\$formattedDate$filename, and it shouldn't exist!"
-            $exitCode = 2
+        if (checkFileExists -path $checkpath -formattedDateTime $formattedDate -remainingFilename $filename) {
+            if ($shouldnotexist -eq $true) {
+                $exitMessage = "CRITICAL: I found the file $checkpath\\$formattedDate$filename, and it shouldn't exist!"
+                $exitCode = 2
+            }
+            else {
+                $exitMessage = "OK: I found the file $checkpath\\$formattedDate$filename"
+                $exitCode = 0
+            }
         }
         else {
-            $exitMessage = "OK: I found the file $checkpath\\$formattedDate$filename"
-            $exitCode = 0
+            if ($shouldnotexist -eq $true) {
+                $exitMessage = "OK: I did not find the file $checkpath\\$formattedDate$filename, and it shouldn't exist."
+                $exitCode = 0
+            }
+            else {
+                $exitMessage = "CRITICAL: I did not find the file, $checkpath\\$formattedDate$filename"
+                $exitCode = 2
+            }
         }
     }
-    else {
-        if ($shouldnotexist -eq $true) {
-            $exitMessage = "OK: I did not find the file $checkpath\\$formattedDate$filename, and it shouldn't exist."
-            $exitCode = 0
-        }
-        else {
-            $exitMessage = "CRITICAL: I did not find the file, $checkpath\\$formattedDate$filename"
-            $exitCode = 2
-        }
-    }
-}
 }
 else
-{   #if the time is not between the stated start and end time then exit as true as you are not wanting to check for a file therefore you don't want to flag Nagios'
+{   
+    # If the time is not between the stated start and end time then exit as true as you are not wanting to check for a file therefore you don't want to flag Nagios'
     $exitMessage = "$formattedTime time is not between $startTime and $EndTime therefore default true"
     $exitCode = 0
 }
-#output relevant info to console/nagios
+
+# Output relevant info to console/nagios
 Write-Output $exitMessage
 exit [int]$exitCode
